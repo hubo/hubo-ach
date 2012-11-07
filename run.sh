dc4a919896bb4578dd523b35d755d890726727dd
Daniel M. Lofaro <dan@danlofaro.com>
2012-11-07 18:36:33 -0500

    added for FT stuff

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@ sudo ifconfig can0 down
 sudo ifconfig can1 down
 sudo ifconfig can2 down
 sudo ifconfig can3 down
-sudo ip link set can0 type can tq 125 prop-seg 1 phase-seg1 2 phase-seg2 4 sjw 1
-sudo ip link set can1 type can tq 125 prop-seg 1 phase-seg1 2 phase-seg2 4 sjw 1
-sudo ip link set can2 type can tq 125 prop-seg 1 phase-seg1 2 phase-seg2 4 sjw 1
-sudo ip link set can3 type can tq 125 prop-seg 1 phase-seg1 2 phase-seg2 4 sjw 1
+sudo echo "i 0x0014 e" > /dev/pcan0
+sudo echo "i 0x0014 e" > /dev/pcan1
+sudo echo "i 0x0014 e" > /dev/pcan2
+sudo echo "i 0x0014 e" > /dev/pcan3
 sudo ifconfig can0 up
 sudo ifconfig can1 up
 sudo ifconfig can2 up
