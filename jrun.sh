af4ef852ed5665dfb1808f46e59831253dd118ec
Daniel M. Lofaro <dan@danlofaro.com>
2012-11-08 22:58:53 -0500

    added just run (jrun.sh) script and setup remote (does not work yet)

--- jrun.sh
+++ jrun.sh
@@ -10,14 +10,6 @@ sudo ifconfig can0 up
 sudo ifconfig can1 up
 sudo ifconfig can2 up
 sudo ifconfig can3 up
-sudo ach -U hubo-ref
-sudo ach -C hubo-ref -m 10 -n 3000
-sudo ach -U hubo-state
-sudo ach -C hubo-state -m 10 -n 3000
-sudo ach -U hubo-init-cmd
-sudo ach -C hubo-init-cmd -m 10 -n 3000
-sudo ach -U hubo-param
-sudo ach -C hubo-param -m 10 -n 3000
 sudo chmod 666 /dev/shm/achshm-hubo-ref
 sudo ./hubo-default
 sudo ./hubo-main 
---
+++ setupRemote.sh
@@ -0,0 +1,2 @@
+chmod 666 /dev/shm/achshm-hubo-ref
+nohup achpipe -Ws hubo-ref -H 192.168.0.10 &
