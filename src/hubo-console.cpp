9ad669771a850f1be761c2a8cd436cbe74b96c3e
Pete Vieira <pete.vieira@gmail.com>
2012-11-11 19:33:45 -0500

    Still trying to push/merge

--- src/hubo-console.cpp
+++ src/hubo-console.cpp
@@ -32,7 +32,11 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdlib.h>
 #include <string.h>
 #include "hubo.h"
-
+// this is because this header file
+// refers to a .c file not a .cpp file
+extern "C" {
+#include "hubo-jointparams.h"
+}
 
 // for ach
 #include <errno.h>
@@ -51,7 +55,6 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <sstream>
 #include <string>
 using namespace std;
-
 
 // ach message type
 //typedef struct hubo h[1];
@@ -78,9 +81,6 @@ void hubo_jmc_beep(struct hubo_param *h, struct hubo_init_cmd *c, char* buff);
 void hubo_jmc_home(struct hubo_param *h, struct hubo_init_cmd *c, char* buff);
 //char* cmd [] ={ "test","hello", "world", "hell" ,"word", "quit", " " };
 void hubo_jmc_home_all(struct hubo_param *h, struct hubo_init_cmd *c, char* buff);
-void setPosZeros();
-//void setConsoleFlags();
-int setDefaultValues(struct hubo_param *h);
 char* cmd [] ={ "initialize","fet","initializeAll","homeAll",
                 "ctrl","enczero", "goto","get","test","update", "quit","beep", "home"," "}; //,
 /*
@@ -129,15 +129,12 @@ int main() {
         ach_put(&chan_hubo_state, &H_state, sizeof(H_state));
 
 	// set default values for H_ref in ach
-	setPosZeros();
+//	setPosZeros();
 
-	// set default values for H_init in ach
-	// this is not working. Not sure if it's really needed
-	// since the structs get initialized with zeros when instantiated
 //	setConsoleFlags();	
 
 	// set default values for Hubo
-	setDefaultValues(&H_param);
+	setJointParams(&H_param);
 
         char *buf;
         rl_attempted_completion_function = my_completion;
@@ -371,107 +368,3 @@ void * xmalloc (int size) {
         }
         return buf;
 }
-
-
-void setPosZeros() {
-        // open ach channel
-//        int r = ach_open(&chan_num, "hubo", NULL);
-//        assert( ACH_OK == r );
-
-        struct hubo_ref H;
-        memset( &H,   0, sizeof(H));
-        size_t fs = 0;
-        int r = ach_get( &chan_hubo_ref, &H, sizeof(H), &fs, NULL, ACH_O_LAST );
-        assert( sizeof(H) == fs );
-
-        int i = 0;
-        for( i = 0; i < HUBO_JOINT_COUNT; i++) {
-                H.ref[i] = 0.0;
-        }
-        ach_put(&chan_hubo_ref, &H, sizeof(H));
-}
-
-void setConsoleFlags() {
-        struct hubo_init_cmd C;
-        memset( &C,   0, sizeof(C));
-        size_t fs =0;
-        int r = ach_get( &chan_hubo_init_cmd, &C, sizeof(C), &fs, NULL, ACH_O_LAST );
-	assert( sizeof(C) == fs );
-	int i = 0;
-        for( i = 0; i < HUBO_JOINT_COUNT; i++ ) {
-                C.cmd[i] = 0;
-                C.val[i] = 0;
-        }
-        r = ach_put(&chan_hubo_init_cmd, &C, sizeof(C));
-	printf("finished setConsoleFlags\n");
-}
-
-int setDefaultValues(struct hubo_param *H) {
-
-        FILE *ptr_file;
-
-        // open file and if fails, return 1
-        if (!(ptr_file=fopen("hubo-config.txt", "r")))
-                return 1;
-
-        struct hubo_joint_param tp;                     //instantiate hubo_jubo_param struct
-        memset(&tp,      0, sizeof(tp));
-      
-	char active[6];
-        char zeroed[6];
-        int j;
-	char buff[100];
-
-	// read in each non-commented line of the config file corresponding to each joint
-	while (fgets(buff, sizeof(buff), ptr_file) != NULL) {
-       		if (buff[0] != '#' && buff[0] != '\n') {
-        		sscanf(buff, "%hu%s%hu%u%hu%hu%hu%hu%hhu%hu%hhu%hhu%hhu%hhu\n", 
-			&tp.jntNo,
-			tp.name,
-			&tp.motNo,  
-			&tp.refEnc, 
-			&tp.drive, 
-			&tp.driven, 
-			&tp.harmonic, 
-			&tp.enc, 
-			&tp.dir,  
-			&tp.jmc, 
-			&tp.can, 
-			&tp.active, 
-			&tp.numMot, 
-			&tp.zeroed);
-
-		// define i to be the joint number
-        	int i = (int)tp.jntNo;
-	
-		//copy contents (all member values) of tp into H.joint[] 
-		//substruct which will populate its member variables
-		memcpy(&(H->joint[i]), &tp, sizeof(tp));        
-		}
-	}
-	// close file stream
-	fclose(ptr_file);
-	
-	// print struct member's values to console to check if it worked
-/*	printf ("printout of setDefaultValues() function in hubo-console.c\n");
-	size_t i;
-	for (i = 0; i < HUBO_JOINT_COUNT; i++) {
-		printf ("%hu\t%s\t%hu\t%u\t%hu\t%hu\t%hu\t%hu\t%hhu\t%hu\t%hhu\t%hhu\t%hhu\t%hhu\n", 
-			H->joint[i].jntNo, 
-			H->joint[i].name,
-			H->joint[i].motNo, 
-			H->joint[i].refEnc, 
-			H->joint[i].drive, 
-			H->joint[i].driven, 
-			H->joint[i].harmonic, 
-			H->joint[i].enc, 
-			H->joint[i].dir, 
-			H->joint[i].jmc, 
-			H->joint[i].can, 
-			H->joint[i].active, 
-			H->joint[i].numMot, 
-			H->joint[i].zeroed);
-	}	
-*/	return 0;
-}
-
