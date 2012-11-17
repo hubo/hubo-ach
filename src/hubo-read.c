91effb6e2f1e5c5fe35c9b0f90b8c297ed0e5458
Daniel M. Lofaro <geovana@hubo-ach-4ch.(none)>
2012-11-17 16:41:03 -0500

    converting to greys work

--- src/hubo-read.c
+++ src/hubo-read.c
@@ -120,6 +120,7 @@ int ftime(struct timeb *tp);
 ach_channel_t chan_hubo_ref;      // hubo-ach
 ach_channel_t chan_hubo_init_cmd; // hubo-ach-console
 ach_channel_t chan_hubo_state;    // hubo-ach-state
+ach_channel_t chan_hubo_param;    // hubo-ach-param
 
 int debug = 0;
 int hubo_debug = 1;
@@ -128,10 +129,10 @@ void huboLoop() {
         // get initial values for hubo
         struct hubo_ref H_ref;
 	struct hubo_state H_state;
+	struct hubo_param H_param;
 	memset( &H_ref,   0, sizeof(H_ref));
 	memset( &H_state, 0, sizeof(H_state));
-	memset( &H_ref,   0, sizeof(H_ref));
-	memset( &H_state, 0, sizeof(H_state));
+	memset( &H_param, 0, sizeof(H_param));
 
         size_t fs;
         //int r = ach_get( &chan_hubo_ref, &H, sizeof(H), &fs, NULL, ACH_O_LAST );
@@ -150,6 +151,15 @@ void huboLoop() {
 		}
 	else{   
 		assert( sizeof(H_state) == fs );
+	 }
+
+	r = ach_get( &chan_hubo_param, &H_param, sizeof(H_param), &fs, NULL, ACH_O_LAST );
+	if(ACH_OK != r) {
+		if(hubo_debug) {
+                       	printf("State ini r = %s\n",ach_result_to_string(r));}
+		}
+	else{   
+		assert( sizeof(H_param) == fs );
 	 }
 
         /* Send a message to the CAN bus */
@@ -203,11 +213,9 @@ void huboLoop() {
 		int jnt = 0;
 		for( i = 0; i < HUBO_JOINT_COUNT; i++) {
 			jnt = i;
-			//if(H_param.joint[jnt].name[0] != 0){
-			if(1==1){
+			if(H_param.joint[jnt].name[0] != 0){
 			printf("%s: Ref = %f \t \t Enc = %f \t Cur = %f \t Tmp = %f\n",
-			//	H_param.joint[jnt].name,
-				"JNT",
+				H_param.joint[jnt].name,
 				H_ref.ref[jnt], 
 				H_state.joint[jnt].pos, 
 				H_state.joint[jnt].cur,
@@ -337,6 +345,8 @@ int main(int argc, char **argv) {
         r = ach_open(&chan_hubo_state, HUBO_CHAN_STATE_NAME , NULL);
         assert( ACH_OK == r );
 	
+	r = ach_open(&chan_hubo_param, HUBO_CHAN_PARAM_NAME , NULL);
+	assert( ACH_OK == r );
         
 	huboLoop();
         pause();
