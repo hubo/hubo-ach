795220c3d0e312898b3fe59d65791f695b031e72
Daniel M. Lofaro <dan@danlofaro.com>
2013-01-19 19:04:49 -0500

    more spring cleaning

--- setupRemote.sh
+++
@@ -1,9 +0,0 @@
-sudo ach -U hubo-ref
-sudo ach -C hubo-ref -m 10 -n 3000
-sudo ach -U hubo-state
-sudo ach -C hubo-state -m 10 -n 3000
-sudo ach -U hubo-init-cmd
-sudo ach -C hubo-init-cmd -m 10 -n 3000
-sudo ach -U hubo-param
-sudo ach -C hubo-param -m 10 -n 3000
-sudo chmod 666 /dev/shm/achshm-hubo-ref
