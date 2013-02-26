a2122595e91b846d14bdef62e6dfa6bb81c68804
Saul Reynolds-Haertle <sreynoldshaertle@gmail.com>
2013-02-26 17:36:36 -0500

    extern "C" when hubo-ach is used in C++ projects.
    This allows c++ projects to use functions from hubo-ach as well as
    data structures, variables, and definitions.

--- include/hubo.h
+++ include/hubo.h
@@ -3,6 +3,11 @@
 #define HUBO_PRIMARY_H
 
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
 // for Hubo
 #include "hubo-daemonID.h"
 
@@ -262,6 +267,13 @@ struct hubo_board_cmd {
 
 
 extern int hubo_debug;
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
 
 #endif //HUBO_PRIMARY_H
 
