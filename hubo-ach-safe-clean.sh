38f104ad845af65d5d5dca52932bb3bd5ae85d7b
Daniel M. Lofaro <dan@danlofaro.com>
2013-03-22 02:39:22 -0400

    fixed some problems with update

--- hubo-ach-safe-clean.sh
+++ hubo-ach-safe-clean.sh
@@ -1,11 +1,11 @@
 hubo-ach killall
-sudo apt-get remove hubo-ach hubo-ach-dev
-sudo apt-get purge hubo-ach hubo-ach-dev
+sudo apt-get --yes --force-yes remove hubo-ach hubo-ach-dev
+sudo apt-get --yes --force-yes purge hubo-ach hubo-ach-dev
 sudo rm /usr/bin/hubo*
 sudo rm /usr/local/bin/hubo*
 sudo rm /usr/include/hubo*
 sudo rm /usr/local/include/hubo*
-sudo rm -R /etc/hubo-ach
 sudo rm /usr/lib/libhuboparams.la
 sudo rm /usr/local/lib/libhuboparams.la
 sudo rm $(locate hubo_ach.py | grep /usr/)
+sudo rm -R /etc/hubo-ach
