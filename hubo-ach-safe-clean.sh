abf1805fdc511555d9f203cddeb6fef34716e888
Daniel M. Lofaro <dan@danlofaro.com>
2013-03-26 15:04:03 -0400

    made hubo-ach-safe-clean more spisific

--- hubo-ach-safe-clean.sh
+++ hubo-ach-safe-clean.sh
@@ -1,11 +1,19 @@
 hubo-ach killall
 sudo apt-get --yes --force-yes remove hubo-ach hubo-ach-dev
 sudo apt-get --yes --force-yes purge hubo-ach hubo-ach-dev
-sudo rm /usr/bin/hubo*
-sudo rm /usr/local/bin/hubo*
-sudo rm /usr/include/hubo*
-sudo rm /usr/local/include/hubo*
-sudo rm /usr/lib/libhuboparams.la
-sudo rm /usr/local/lib/libhuboparams.la
-sudo rm $(locate hubo_ach.py | grep /usr/)
-sudo rm -R /etc/hubo-ach
+sudo updatedb
+sudo rm $(locate hubo-daemon | grep /bin)
+sudo rm $(locate hubo-console | grep /bin)
+sudo rm $(locate hubo-read | grep /bin)
+sudo rm $(locate hubo-ach | grep /bin)
+sudo rm /etc/hubo-ach/joint.table
+sudo rm /etc/hubo-ach/sensor.table
+sudo rm /etc/hubo-ach/virtualHubo.py
+sudo rm /etc/hubo-ach/hubo-ach-safe-clean.sh
+sudo rm $(locate libhuboparams.so | grep /usr)
+sudo rm $(locate hubo_ach.pyc | grep /usr)
+sudo rm $(locate hubo_ach.pyo | grep /usr)
+sudo rm $(locate hubo_ach.py | grep /usr)
+sudo rm $(locate hubo.h | grep /usr)
+sudo rm $(locate hubo-daemonID.h | grep /usr)
+sudo rm $(locate hubo-jointparams.h | grep /usr)
